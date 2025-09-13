4f0b649b970d2711aae06342e669b067316d34a8
Paras Raba <parasgadhvi12345@gmail.com>
2025-09-13 23:44:14 +0530

    feat: add api for /applicable-coupons
    - Current implementation is not working for bxgy coupons
    - working for other 2 coupons

--- cmd/main.go
+++ cmd/main.go
@@ -8,6 +8,7 @@ import (
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
 
+	"github.com/ParasRaba155/monk-commerce-task/cart"
 	"github.com/ParasRaba155/monk-commerce-task/coupon"
 )
 
@@ -21,12 +22,15 @@ func main() {
 
 	repo := coupon.NewRepository()
 	couponHandler := coupon.NewHandler(repo)
+	cartHandler := cart.NewHandler(repo)
 
 	e.POST("/coupons", couponHandler.Create)
 	e.GET("/coupons", couponHandler.Get)
 	e.GET("/coupons/:id", couponHandler.GetByID)
 	e.PUT("/coupons/:id", couponHandler.UpdateByID)
 	e.DELETE("/coupons/:id", couponHandler.DeleteByID)
+
+	e.POST("/applicable-coupon", cartHandler.ApplicableCoupon)
 
 	// Start server
 	if err := e.Start(":8080"); err != nil && !errors.Is(err, http.ErrServerClosed) {
